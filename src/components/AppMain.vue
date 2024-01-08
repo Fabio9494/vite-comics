a234f3689144133e0a7694b40a19f7e41efcfe56
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-08 19:23:39 +0100

    add main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template lang="">
+    <div >
+        -- content goes here -- 
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+    background-color: black;
+    color: white;
+    font-weight: bold;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+}
+</style>
