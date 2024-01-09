6499429f2963a7766db0aeff4bb403774b856fc6
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-09 17:14:00 +0100

    create componet  CardFumetti

---
+++ src/components/CardFumetti.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'CardFumetti',
+    props: {
+        fumetto: Object
+    }
+}
+</script>
+
+<template lang="">
+    <div class="card" :key="index">
+        <img :src="fumetto.thumb" alt="">
+        <h5>{{fumetto.series}}</h5>
+    </div>
+</template>
+<style lang="scss" scoped>
+.card {
+    width: calc(100% / 6 - 20px);
+    color: white;
+
+    img {
+        width: 80%;
+        height: 200px;
+    }
+}
+</style>
